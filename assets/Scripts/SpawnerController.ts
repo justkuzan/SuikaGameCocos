61e3ee58c57025970f8bcdccd15b0baaceb5c610
Anton Kuzan <antonkuzan@gmail.com>
2026-08-09 21:59:40 +0300

    fix: spawnController

--- assets/Scripts/SpawnerController.ts
+++ assets/Scripts/SpawnerController.ts
@@ -8,19 +8,40 @@ import {
   Node,
   Sprite,
   SpriteComponent,
+  UITransform,
+  Vec2,
+  Vec3,
 } from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("SpawnerController")
 export class SpawnerController extends Component {
+  private _tempUIVec3: Vec3 = new Vec3(0, 0, 0);
+  private _tempLocalVec3: Vec3 = new Vec3(0, 0, 0);
+
   protected onLoad(): void {
-    input.on(Input.EventType.TOUCH_START, this.onTouchStart, this);
+    // input.on(Input.EventType.TOUCH_START, this.onTouchStart, this);
     // input.on(Input.EventType.TOUCH_MOVE, this.onTouchStart, this);
     input.on(Input.EventType.MOUSE_MOVE, this.onMouseMove, this);
   }
   protected onDestroy(): void {
-    input.off(Input.EventType.TOUCH_START, this.onTouchStart, this);
+    // input.off(Input.EventType.TOUCH_START, this.onTouchStart, this);
     // input.off(Input.EventType.TOUCH_MOVE, this.onTouchStart, this);
     input.off(Input.EventType.MOUSE_MOVE, this.onMouseMove, this);
+  }
+
+  onMouseMove(event: EventMouse) {
+    this.upgradePosition(event);
+  }
+
+  upgradePosition(event: EventMouse) {
+    const currentUIPosition = event.getUILocation();
+    this._tempUIVec3.set(currentUIPosition.x, currentUIPosition.y);
+
+    this.node
+      .getComponent(UITransform)
+      .convertToNodeSpaceAR(this._tempUIVec3, this._tempLocalVec3);
+
+    this.node.setPosition(this._tempLocalVec3);
   }
 }
